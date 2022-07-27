84c7d41fa61d28e45727e475fec64a56e1024657
unknown <maximtimchenko746@gmail.com>
2022-07-27 16:31:58 +0300

    add logic

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+const root = document.querySelector('#root'),
+      city = document.querySelector('#city'),
+      popup = document.querySelector('#popup'),
+      popupClose = document.querySelector('.popup-close');
+
+const form = document.querySelector('#form'),
+      formInput = document.querySelector('#text-input');     
 
 
-const linkApi = 'http://api.weatherstack.com/current?access_key=558e440c9ad8a11d427897d483ecf686'
+
+const linkApi = 'http://api.weatherstack.com/current?access_key=333b9a3c6d7430cff08ff39f3f94beeb'
 
 let storage = {
     city: 'London',
@@ -42,6 +50,82 @@ const getDataApi = async () => {
         humidity,
         cloudcover
     }
+
+    render();
+
+    
 } 
+
+const htmlComponent = () => {  
+    const {city, weatherIcon, temperature, weatherDescription, observationTime,windSpeed, humidity,cloudcover} = storage
+
+    return  ` <div class="container">
+        <header>
+            <div class="city-title" id="city">${city}</div>
+        </header>
+        <main class="city-info">
+            <div class="weather-icon">
+                <img src="${weatherIcon}" alt="${weatherDescription}" width="150px" height="150px">
+            </div>
+            <div class="city-description">
+                <span class="weather-temp">${temperature}°</span>
+                <span class="weather-description">${weatherDescription}</span>
+                <span class="weather-date">${observationTime}</span>
+            </div>
+
+            <div class="city-elements">
+                <div class="wind element" >
+                    <img src="img/icons/wind.png" alt="">
+                    <span class="element-speed">${windSpeed} km/h</span>
+                    <span class="element-name">Wind</span>
+                </div>
+                <div class="humidity element" >
+                    <img  src="img/icons/humidity.png" alt="">
+                    <span class="element-speed">${humidity}%</span>
+                    <span class="element-name">humidity</span>
+                </div>
+                <div class="rain element" >
+                    <img src="img/icons/gauge.png" alt="">
+                    <span class="element-speed">${cloudcover}%</span>
+                    <span class="element-name">Rain</span>
+                </div>
+            </div>
+
+        </main>
+      </div>
+    </div>
+`}
+const toggleClass = () => {
+    popup.classList.toggle("active")
+}
+
+popupClose.addEventListener('click', toggleClass)
+
+const render = () => {
+    root.innerHTML = htmlComponent();
+
+
+    const city = document.querySelector('#city')
+    city.addEventListener('click', toggleClass)
+};
+
+const setInputValue = (e) => {
+    storage = {
+        ...storage,
+        city: e.target.value
+    }
+}
+
+const submitData = (e) => {
+    e.preventDefault();
+    getDataApi();
+
+    toggleClass();
+}
+
+
+form.addEventListener('submit', submitData)
+formInput.addEventListener('input', setInputValue)
+
 
 getDataApi()
